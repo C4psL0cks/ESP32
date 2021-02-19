6adbd69bbc2d4cbbfedf0f3a3a1e223bdfa161ac
unknown <ox03795@hotmail.com>
2021-02-19 18:10:44 +0700

    update

---
+++ EC25E-WIFI/EC25E-WIFI.ino
@@ -0,0 +1,28 @@
+#include <WiFi.h>
+#define WIFI_SSID   "dlink-ABD0"
+#define WIFI_PASS   "yyafr68490"
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("-------------------------------------");
+  Serial.println("Running!");
+  Serial.println("-------------------------------------");
+  // Connect to Wifi.
+  Serial.println();
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.printf("WiFi Connecting to %s\n", WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.printf("\nWiFi connected\nIP : ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
